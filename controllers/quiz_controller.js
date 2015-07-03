b930fa762a3e90f7144d9c811f2a7b7c5446e963
aprendizcella <aprendizcella@gmail.com>
2015-07-03 17:47:31 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -4,26 +4,33 @@ var models = require('../models/models.js');
 // En las nuevas versiones de Sequelize (3 o más) el interfaz ha cambiado para usar "Promesas". A grandes rasgos hay que cambiar .success por .then, .error por .catch y .done por .finally
 // Otra modificación a tener en cuenta findAll se queda igual pero hay que cambiar find por findById
 
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(function(quiz) {
+    if(quiz) {
+      req.quiz = quiz;
+      next();
+    } else { next(new Error('No existe quizId=' + quizId));}
+  })
+  .catch(function(error) {next(error);});
+};
+
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes){
     res.render('quizes/index', {quizes: quizes});  
-  });
+  }
+  ).catch(function(error) { next(error);});
 };
 
 exports.show = function(req, res) {
-  models.Quiz.findById(req.params.quizId).then(function(quiz){
-    res.render('quizes/show', {quiz: quiz});  
-  });
+  res.render('quizes/show', { quiz: req.quiz});
 };
 
 exports.answer = function(req, res) {
-  models.Quiz.findById(req.params.quizId).then(function(quiz){
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
-    } else {
-      res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
-    }
-  });
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
 // GET /author
