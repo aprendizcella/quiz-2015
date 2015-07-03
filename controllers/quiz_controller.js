cec6c663e20f2123f0d87f0b5f878006980cef89
aprendizcella <aprendizcella@gmail.com>
2015-07-03 17:26:28 +0200

    Lista de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,23 +2,28 @@ var models = require('../models/models.js');
 
 // Foros de Miriada X error en models.js 
 // En las nuevas versiones de Sequelize (3 o más) el interfaz ha cambiado para usar "Promesas". A grandes rasgos hay que cambiar .success por .then, .error por .catch y .done por .finally
+// Otra modificación a tener en cuenta findAll se queda igual pero hay que cambiar find por findById
 
-// GET /quizes/question
-exports.question = function(req, res) {
-  models.Quiz.findAll().then(function(quiz) {
-    res.render('quizes/question', { pregunta: quiz[0].pregunta});
-  })
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes){
+    res.render('quizes/index', {quizes: quizes});  
+  });
 };
 
-// GET /quizes/answer
+exports.show = function(req, res) {
+  models.Quiz.findById(req.params.quizId).then(function(quiz){
+    res.render('quizes/show', {quiz: quiz});  
+  });
+};
+
 exports.answer = function(req, res) {
-  models.Quiz.findAll().then(function(quiz) {
-    if (req.query.respuesta === quiz[0].respuesta) {
-      res.render('quizes/answer', { respuesta: 'Correcto' });
+  models.Quiz.findById(req.params.quizId).then(function(quiz){
+    if (req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
     } else {
-      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+      res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
     }
-  })
+  });
 };
 
 // GET /author
